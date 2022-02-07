0d88d3c4ac23bee9ee4dfd994e33da23cae2e941
Mike Hamburg <mike@shiftleft.org>
2022-02-07 13:12:24 +0100

    add lfr_file.h

--- src/lfr_file.c
+++ src/lfr_file.c
@@ -5,3 +5,4 @@
  * Maps to/from files
  */
 
+#include "lfr_file.h"
---
+++ src/lfr_file.h
@@ -0,0 +1,30 @@
+/** @file lfr_file.c
+ * @author Mike Hamburg
+ * @copyright 2020-2022 Rambus Inc.
+ *
+ * Maps to/from files
+ */
+
+#ifndef __LFR_FILE_H__
+#define __LFR_FILE_H__
+
+#include "lfr_uniform.h"
+#include "lfr_nonuniform.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+
+#ifdef __cplusplus
+} // extern "C"
+
+namespace LibFrayed {
+}
+#endif /* __cplusplus */
+#endif /* __LFR_FILE_H__ */
+
